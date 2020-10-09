20af086da16ccdc606bfb2bbfe3195137159e012
aljailaa <aljailaniamin@gmail.com>
2020-10-08 22:00:45 -0400

    add player implementation

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,19 @@
-
 public class Player {
 
+	private String name;
+
+	public Player(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return What the die returns for this use
+	 */
+	public int takeTurn(Die die) {	
+		return die.roll();
+	}
+
+	public String toString() {
+		return name;
+	}
 }
