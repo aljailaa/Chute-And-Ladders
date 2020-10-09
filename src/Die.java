f99e0b305032b89caff6b575978787859506de23
aljailaa <aljailaniamin@gmail.com>
2020-10-08 21:46:30 -0400

    die fuction implementation

--- src/Die.java
+++ src/Die.java
@@ -1,4 +1,21 @@
+import java.util.Random;
 
 public class Die {
 
+	private Random rand;
+	
+	/**
+	 * Initialize the fields
+	 */
+	public Die(){
+		rand = new Random();
+	}
+	
+	
+	/**
+	 * @return returns a random value between 1 and 6
+	 */
+	public int roll(){
+		return rand.nextInt(6)+1;
+	}
 }
